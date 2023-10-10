a86f5526b8acd0030a20142675159b058ca981ec
JonatasTB <101901086+jonatastb@users.noreply.github.com>
2023-10-10 20:05:35 -0300

    :tada: init: First Commit in Repo

---
+++ todo/src/components/TodoForm.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+
+const TodoForm = ({addTodo}) => {
+  const [value, setValue] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value || !category) return;
+
+    addTodo(value, category);
+
+    setValue("");
+    setCategory("");
+  };
+
+  return (
+    <div className="todo-form">
+      <h2>Criar tarefa:</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Digite o Título' value={value} onChange={(e) => {setValue(e.target.value)}} />
+        <select name="" id="" value={category} onChange={(e) => {setCategory(e.target.value)}}>
+          <option value="">Selecione uma categoria</option>
+          <option value="Trabalho">Trabalho</option>
+          <option value="Escola">Escola</option>
+          <option value="Pessoal">Pessoal</option>
+        </select>
+        <button type="submit">Criar</button>
+      </form>
+    </div>
+  );
+};
+
+export default TodoForm;
